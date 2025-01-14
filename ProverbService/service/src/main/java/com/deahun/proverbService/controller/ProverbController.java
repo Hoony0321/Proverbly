98f84026e2eb47b12b8863d984f011b739e693a4
Hoony0321 <eogns0321@gmail.com>
2025-01-13 18:17:40 -0800

    feat : proverb service에 id로 정보 가져오는 api 추가

--- ProverbService/service/src/main/java/com/deahun/proverbService/controller/ProverbController.java
+++ ProverbService/service/src/main/java/com/deahun/proverbService/controller/ProverbController.java
@@ -3,8 +3,10 @@ package com.deahun.proverbService.controller;
 import com.deahun.proverbService.domain.Proverb;
 import com.deahun.proverbService.service.ProverbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,6 +29,12 @@ public class ProverbController {
     public Object getRandomProverb(){
         Proverb randomProverb = proverbService.findRandomProverb();
         return randomProverb;
+    }
+
+    @GetMapping("proverb/{id}")
+    public Object getProverbById(@PathVariable("id") Long id) {
+        Proverb proverb = proverbService.findProverbById(id);
+        return proverb;
     }
 
 
--- ProverbService/service/src/main/java/com/deahun/proverbService/service/ProverbService.java
+++ ProverbService/service/src/main/java/com/deahun/proverbService/service/ProverbService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -22,5 +23,14 @@ public class ProverbService {
         List<Proverb> allProverb = findAllProverb();
         int randomIndex = (int)(Math.random() * allProverb.size());
         return allProverb.get(randomIndex);
+    }
+
+    public Proverb findProverbById(Long id) {
+        Optional<Proverb> findOne = proverbRepository.findById(id);
+        if(findOne.isEmpty()) {
+            throw new IllegalArgumentException("해당 id에 해당하는 속담이 없습니다.");
+        }
+
+        return findOne.get();
     }
 }
